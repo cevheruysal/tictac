95b71403ad1ae5f9c1687bc492beeabfa200bb7a
A Cevher Uysal <ahmetcevheruysal@gmail.com>
2026-07-06 11:06:34 +0200

    HW3--impl-diode-and-bjt-stamp-logic

--- src/shared/opentictac/netlist/devices/nonlin/SimpleBJT.cpp
+++ src/shared/opentictac/netlist/devices/nonlin/SimpleBJT.cpp
@@ -13,7 +13,7 @@ SimpleBJT::SimpleBJT(std::vector<MYINT> & pins,
       alpha_f_(params[0]),
       alpha_r_(params[1]),
       I_es_(params[2]),
-      I_cs_(I_es_ + alpha_f_ / alpha_r_),
+      I_cs_(I_es_ * alpha_f_ / alpha_r_),
       v_t_(params[3]),
       v_t_inv_(1. / v_t_)
 {
@@ -39,7 +39,15 @@ void
   if (nrStampsElements > (MYINT)potentialEntryValues.size()) potentialEntryValues.resize(nrStampsElements, 0.0);
 
  /* === HERE STARTS THE CODE OF ASSIGNMENT: 3 ==== */ 
- 
+  stampColumn[0] = 0; stampRow[0] = 0; entryType[0] = STATIC_ONLY; potentialEntryValues[0] = 0.0;
+  stampColumn[1] = 1; stampRow[1] = 0; entryType[1] = STATIC_ONLY; potentialEntryValues[1] = 0.0;
+  stampColumn[2] = 2; stampRow[2] = 0; entryType[2] = STATIC_ONLY; potentialEntryValues[2] = 0.0;
+  stampColumn[3] = 0; stampRow[3] = 1; entryType[3] = STATIC_ONLY; potentialEntryValues[3] = 0.0;
+  stampColumn[4] = 1; stampRow[4] = 1; entryType[4] = STATIC_ONLY; potentialEntryValues[4] = 0.0;
+  stampColumn[5] = 2; stampRow[5] = 1; entryType[5] = STATIC_ONLY; potentialEntryValues[5] = 0.0;
+  stampColumn[6] = 0; stampRow[6] = 2; entryType[6] = STATIC_ONLY; potentialEntryValues[6] = 0.0;
+  stampColumn[7] = 1; stampRow[7] = 2; entryType[7] = STATIC_ONLY; potentialEntryValues[7] = 0.0;
+  stampColumn[8] = 2; stampRow[8] = 2; entryType[8] = STATIC_ONLY; potentialEntryValues[8] = 0.0;
  /* === HERE ENDS THE CODE OF ASSIGNMENT: 3 ==== */ 
 }
 
@@ -52,7 +60,14 @@ void
 {
 
  /* === HERE STARTS THE CODE OF ASSIGNMENT: 3 ==== */ 
-
+  nrRHSStampsElements = 3;
+  if (nrRHSStampsElements > (MYINT)stampColumn.size()) stampColumn.resize(nrRHSStampsElements, -1);
+  if (nrRHSStampsElements > (MYINT)entryType.size()) entryType.resize(nrRHSStampsElements);
+  if (nrRHSStampsElements > (MYINT)potentialEntryValues.size()) potentialEntryValues.resize(nrRHSStampsElements, 0.0);
+  
+  stampColumn[0] = 0; entryType[0] = STATIC_ONLY; potentialEntryValues[0] = 0.0;
+  stampColumn[1] = 1; entryType[1] = STATIC_ONLY; potentialEntryValues[1] = 0.0;
+  stampColumn[2] = 2; entryType[2] = STATIC_ONLY; potentialEntryValues[2] = 0.0;
  /* === HERE ENDS THE CODE OF ASSIGNMENT: 3 ==== */ 
 }
 
@@ -71,7 +86,46 @@ void
 {
 
  /* === HERE STARTS THE CODE OF ASSIGNMENT: 3 ==== */ 
+  MYREAL mfact = (MYREAL)this->getMFactor();
+  nrRHSStamp = 3;
+  nrMatrixStamp = 9;
 
+  MYREAL vC = localVars[0];
+  MYREAL vB = localVars[1];
+  MYREAL vE = localVars[2];
+  
+  MYREAL expF = exp((vB - vE) * v_t_inv_);
+  MYREAL expR = exp((vB - vC) * v_t_inv_);
+  
+  MYREAL IF = I_es_ * (expF - 1.0);
+  MYREAL IR = I_cs_ * (expR - 1.0);
+  
+  MYREAL dIF_dvB = I_es_ * v_t_inv_ * expF;
+  MYREAL dIF_dvE = -dIF_dvB;
+  MYREAL dIF_dvC = 0.0;
+
+  MYREAL dIR_dvB = I_cs_ * v_t_inv_ * expR;
+  MYREAL dIR_dvC = -dIR_dvB;
+  MYREAL dIR_dvE = 0.0;
+  
+  // Row 0: C -> residual +IC = alpha_f*IF - IR
+  matrixValues[0] = (-dIR_dvC) * mfact;
+  matrixValues[1] = (alpha_f_ * dIF_dvB - dIR_dvB) * mfact;
+  matrixValues[2] = (alpha_f_ * dIF_dvE) * mfact;
+
+  // Row 1: B -> residual +IB = (1-alpha_f)*IF + (1-alpha_r)*IR
+  matrixValues[3] = ((1.0 - alpha_r_) * dIR_dvC) * mfact;
+  matrixValues[4] = ((1.0 - alpha_f_) * dIF_dvB + (1.0 - alpha_r_) * dIR_dvB) * mfact;
+  matrixValues[5] = ((1.0 - alpha_f_) * dIF_dvE) * mfact;
+
+  // Row 2: E -> residual -IE = -(IF - alpha_r*IR)
+  matrixValues[6] = (alpha_r_ * dIR_dvC) * mfact;
+  matrixValues[7] = (alpha_r_ * dIR_dvB - dIF_dvB) * mfact;
+  matrixValues[8] = (-dIF_dvE) * mfact;
+
+  rhsValues[0] = (alpha_f_ * IF - IR) * mfact;
+  rhsValues[1] = (IF * (1.0 - alpha_f_) + IR * (1.0 - alpha_r_)) * mfact;
+  rhsValues[2] = (alpha_r_ * IR - IF) * mfact;
  /* === HERE ENDS THE CODE OF ASSIGNMENT: 3 ==== */ 
 }
 
--- src/shared/opentictac/netlist/devices/nonlin/SimpleDiode.cpp
+++ src/shared/opentictac/netlist/devices/nonlin/SimpleDiode.cpp
@@ -34,7 +34,10 @@ void
   if (nrStampsElements > (MYINT)potentialEntryValues.size()) potentialEntryValues.resize(nrStampsElements, 0.0);
 
  /* === HERE STARTS THE CODE OF ASSIGNMENT: 3 ==== */ 
- 
+  stampColumn[0] = 0; stampRow[0] = 0; entryType[0] = STATIC_ONLY; potentialEntryValues[0] = 0.0;
+  stampColumn[1] = 1; stampRow[1] = 0; entryType[1] = STATIC_ONLY; potentialEntryValues[1] = 0.0;
+  stampColumn[2] = 0; stampRow[2] = 1; entryType[2] = STATIC_ONLY; potentialEntryValues[2] = 0.0;
+  stampColumn[3] = 1; stampRow[3] = 1; entryType[3] = STATIC_ONLY; potentialEntryValues[3] = 0.0;
  /* === HERE ENDS THE CODE OF ASSIGNMENT: 3 ==== */ 
 }
 
@@ -47,7 +50,13 @@ void
 {
 
  /* === HERE STARTS THE CODE OF ASSIGNMENT: 3 ==== */ 
-
+  nrRHSStampsElements = 2;
+  if (nrRHSStampsElements > (MYINT)stampColumn.size()) stampColumn.resize(nrRHSStampsElements, -1);
+  if (nrRHSStampsElements > (MYINT)entryType.size()) entryType.resize(nrRHSStampsElements);
+  if (nrRHSStampsElements > (MYINT)potentialEntryValues.size()) potentialEntryValues.resize(nrRHSStampsElements, 0.0);
+  
+  stampColumn[0] = 0; entryType[0] = STATIC_ONLY; potentialEntryValues[0] = 0.0;
+  stampColumn[1] = 1; entryType[1] = STATIC_ONLY; potentialEntryValues[1] = 0.0;
  /* === HERE ENDS THE CODE OF ASSIGNMENT: 3 ==== */ 
 }
 
@@ -64,7 +73,24 @@ void
 {
 
  /* === HERE STARTS THE CODE OF ASSIGNMENT: 3 ==== */ 
- 
+  MYREAL mfact = (MYREAL)this->getMFactor();
+  nrRHSStamp = 2;
+  nrMatrixStamp = 4;
+  
+  MYREAL vp = localVars[0];
+  MYREAL vn = localVars[1];
+  MYREAL vDiff = vp - vn;
+  
+  MYREAL ID = getCurrent(vDiff);
+  MYREAL dID = i_s_ * v_t_inv_ * exp(vDiff * v_t_inv_);
+  
+  matrixValues[0] = dID * mfact;
+  matrixValues[1] = -dID * mfact;
+  matrixValues[2] = -dID * mfact;
+  matrixValues[3] = dID * mfact;
+  
+  rhsValues[0] = ID * mfact;
+  rhsValues[1] = -ID * mfact;
  /* === HERE ENDS THE CODE OF ASSIGNMENT: 3 ==== */ 
 }
 
